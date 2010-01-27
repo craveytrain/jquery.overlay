8c51705cde0f1576e2fe39da0d611f18811c079a
Mike Cravey <techie@craveytrain.com>
2010-01-26 23:14:26 -0600

    adding js and css files

---
+++ jquery.labelOverlay.js
@@ -0,0 +1,59 @@
+(function($) {
+// Overlay label on top of input
+$.fn.labelOverlay = function(options) {
+  var opts = $.extend({}, $.fn.labelOverlay.defaults, options);
+
+  return this.each(function() {
+    var $this = $(this);
+
+    // Support for the Metadata Plugin.
+    var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
+
+	o.placeholder = $.fn.labelOverlay.support();
+
+	if(!o.placeholder) {
+		$this.data('id', $this.attr('id'));
+		var $label = $('label[for=' + $this.data('id') + ']');
+		$label.addClass(o.overlayedClass);
+		if (!$this.val() === '') {
+			label.hide();
+		}
+		$this
+			.focus(function(e){
+				var $this = $(e.target), $label = $('label[for=' + $this.data('id') + ']');
+				$label.hide();
+			})
+			.blur(function(e){
+				var $this = $(e.target), $label = $('label[for=' + $this.data('id') + ']');
+				if ($this.val() == '') {
+					$label.show();						
+				}
+			})
+		;
+	}
+  });
+
+  // private function for debugging
+  function debug($obj) {
+    if (window.console && window.console.log) {
+      window.console.log($obj);
+    }
+  }
+};
+
+$.fn.labelOverlay.support = function() {
+	if (window.Modernizr) {
+		return Modernizr.input.placeholder;
+	} else {
+	  var i = document.createElement('input');
+	  return 'placeholder' in i;	
+	}
+}
+
+// default options
+$.fn.labelOverlay.defaults = {
+	placeholder: false,
+	overlayedClass: 'overlayed'
+};
+
+})(jQuery);
