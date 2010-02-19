48a847572eb0629dde460efcedaf6c39b91a929e
Mike Cravey <techie@craveytrain.com>
2010-02-19 10:38:07 -0600

    added ability to pass a string and build a new node for the overlay.

--- jquery.labelOverlay.js
+++ jquery.labelOverlay.js
@@ -10,36 +10,49 @@
  */
 
 ;(function($) {
-$.fn.labelOverlay = function(options) {
-  var opts = $.extend({}, $.fn.labelOverlay.defaults, options);
+$.fn.overlay = function(options) {
+  var opts = $.extend(true, {}, $.fn.overlay.defaults, options);
 
   return this.each(function() {
     var $this = $(this);
 
     // Support for the Metadata Plugin.
-    var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
+    var o = $.meta ? $.extend(true, {}, opts, $this.data()) : opts;
 
-	o.placeholderSupport = $.fn.labelOverlay.support();
+	o.placeholderSupport = $.fn.overlay.support();
 
 	if(!o.placeholderSupport) {
-		$this.data('id', $this.attr('id'));
-		var $label = $('label[for=' + $this.data('id') + ']');
-		$label.addClass(o.overlayedClass);
-		if (!$this.val() === '') {
-			label.hide();
+		var $overlay;
+		if (o.overlay.txt) {
+			if ($(o.overlay.txt).length) {
+				$overlay = o.overlay.txt;
+			} else {
+				$overlay = $('<div />').text(o.overlay.txt).appendTo($this.parent());
+			}
+		} else {
+			$overlay = $('label[for=' + $this.attr('id') + ']');
+		}
+		$overlay.attr(o.overlay.attrs).data('input', $this);
+		if (!$this.val() == '') {
+			$overlay.hide();
 		}
 		$this
+			.data('overlay', $overlay)
 			.focus(function(e){
-				var $this = $(e.target), $label = $('label[for=' + $this.data('id') + ']');
-				$label.hide();
+				var $this = $(e.target), $overlay = $this.data('overlay');
+				$overlay.hide();
 			})
 			.blur(function(e){
-				var $this = $(e.target), $label = $('label[for=' + $this.data('id') + ']');
+				var $this = $(e.target), $overlay = $this.data('overlay');
 				if ($this.val() == '') {
-					$label.show();						
+					$overlay.show();						
 				}
 			})
 		;
+		$overlay.click(function(e) {
+			var $this = $(e.target), $input = $this.data('input');
+			$input.focus();
+		});
 	}
   });
 
@@ -51,19 +64,24 @@ $.fn.labelOverlay = function(options) {
   }
 };
 
-$.fn.labelOverlay.support = function() {
+$.fn.overlay.support = function() {
 	if (window.Modernizr) {
 		return Modernizr.input.placeholder;
 	} else {
 	  var i = document.createElement('input');
 	  return 'placeholder' in i;	
 	}
-}
+};
 
 // default options
-$.fn.labelOverlay.defaults = {
+$.fn.overlay.defaults = {
 	placeholderSupport: false,
-	overlayedClass: 'overlayed'
+	overlay: {
+		txt: null,
+		attrs: {
+			class: 'overlayed'
+		}
+	}
 };
 
 })(jQuery);
